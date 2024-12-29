58a520db6ecc21831aaa0634f1b627667f06a4f8
Daria Zakirova <daria.zakirova@gmail.com>
2024-12-29 11:15:41 +0200

    add format option

--- gendiff.js
+++ gendiff.js
@@ -7,5 +7,6 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format');
 
-program.parse();
+program.parse(process.argv);
