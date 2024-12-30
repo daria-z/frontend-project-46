8233a336f1cc17f4b1859b834ffe2a5996f2681a
Daria Zakirova <daria.zakirova@gmail.com>
2024-12-30 09:04:00 +0200

    temp variant

--- gendiff.js
+++ gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-
+import fs from 'fs';
+import path from 'path';
 import { Command } from 'commander';
 const program = new Command();
 
@@ -7,6 +8,15 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .arguments('<path1> <path2>')
+  .action((path1, path2) => {
+    const file1 = fs.readFileSync(path1, 'utf-8');
+    const file2 = fs.readFileSync(path2, 'utf-8');
+    console.log(path.resolve(process.cwd(), path1));
+    console.log(file1);
+    // console.log(path.resolve(process.cwd(), path2));
+    console.log(file2);
+  });
 
 program.parse(process.argv);
