82df2c7ceea0aa9d0d373ea9a1e86a6b2efbd1c0
Daria Zakirova <daria.zakirova@gmail.com>
2025-01-11 08:52:38 +0200

    fix gendiffCli

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,12 +1,9 @@
 import parseFiles from './parsers.js';
 import findDiff from './findDiff.js';
+import fileReader from './fileReader.js';
 
 export default (path1, path2) => {
-  const [file1, file2] = parseFiles(path1, path2);
-
-  if (!file1 || !file2) {
-    return null;
-  }
+  const [file1, file2] = parseFiles(fileReader(path1, path2));
 
   return findDiff(file1, file2);
 };
