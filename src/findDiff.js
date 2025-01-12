eefb7868dc5419d886620ed281c3c66939e22345
Daria Zakirova <daria.zakirova@gmail.com>
2025-01-12 17:45:33 +0200

    fix findDiff

--- src/findDiff.js
+++ src/findDiff.js
@@ -7,42 +7,39 @@ const findDiff = (obj1, obj2) => {
     const isDeleted = keys(obj1).includes(key);
     const isAdded = keys(obj2).includes(key);
 
-    if (isAdded && isDeleted) {
-      if (isPlainObject(obj1[key]) && isPlainObject(obj2[key])) {
-        return {
-          ...result,
-          [key]: { status: 'object', value: findDiff(obj1[key], obj2[key]) },
-
-        };
-      }
-
-      if (obj1[key] === obj2[key]) {
-        return {
-          ...result,
-          [key]: { status: 'unchanged', value: obj1[key] },
-        };
-      }
-
-      if (obj1[key] !== obj2[key]) {
-        return {
-          ...result,
-          [key]: { status: 'changed', oldValue: obj1[key], newValue: obj2[key] },
-        };
-      }
-      return result;
-    }
-
-    if (isAdded) {
+    if (isAdded && !isDeleted) {
       return {
         ...result,
         [key]: { status: 'added', value: obj2[key] },
       };
     }
 
-    if (isDeleted) {
+    if (isDeleted && !isAdded) {
       return {
         ...result,
         [key]: { status: 'deleted', value: obj1[key] },
+      };
+    }
+
+    if (isPlainObject(obj1[key]) && isPlainObject(obj2[key])) {
+      return {
+        ...result,
+        [key]: { status: 'object', value: findDiff(obj1[key], obj2[key]) },
+
+      };
+    }
+
+    if (obj1[key] === obj2[key]) {
+      return {
+        ...result,
+        [key]: { status: 'unchanged', value: obj1[key] },
+      };
+    }
+
+    if (obj1[key] !== obj2[key]) {
+      return {
+        ...result,
+        [key]: { status: 'changed', oldValue: obj1[key], newValue: obj2[key] },
       };
     }
     return result;
